3d9ffec0a73699b1d85c5f39c55ae2995d470fab
1142902142@qq.com <1142902142@qq.com>
2020-09-05 10:30:58 +0800

    add txt

---
+++ src/main/java/common/math.java
@@ -0,0 +1,10 @@
+package common;
+
+public class math {
+    public static void main(String[] arg){
+        test();
+    }
+    public static void test(){
+        System.out.println(1.0/0.0);
+    }
+}
